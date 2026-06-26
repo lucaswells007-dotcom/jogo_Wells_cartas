5a94ea30401e8eb3854781d637a2f5f8542a72f6
lucaswells007-dotcom <lucaswells007@gmail.com>
2026-06-26 18:42:19 -0300

    u0pfate

--- views/pagina_principal.py
+++ views/pagina_principal.py
@@ -2,7 +2,7 @@ from scripts.auxiliar import consultar_juiz
 import streamlit as st
 st.write('pagina principal')
 
-pergunta = st.chat_input("Say something")
+pergunta = st.chat_input("Say something",accept_audio=True)
 
 if pergunta:
     resultado = consultar_juiz(pergunta)
