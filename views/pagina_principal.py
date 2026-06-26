69835a032e8d0eece628c7482ea3fe521a553bbf
lucaswells007-dotcom <lucaswells007@gmail.com>
2026-06-26 18:33:58 -0300

    udate

--- views/pagina_principal.py
+++ views/pagina_principal.py
@@ -1,6 +1,6 @@
 from scripts.auxiliar import consultar_juiz
 import streamlit as st
-st.write('testes')
+st.write('pagina principal')
 
 if st.button('testes'):
     resultado = consultar_juiz('quem vai ganhar a copa do mundo?')
