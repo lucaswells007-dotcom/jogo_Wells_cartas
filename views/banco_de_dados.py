bb54da7ba1ebc506de3a8ea1e3f7a4bd650afa3e
lucaswells007-dotcom <lucaswells007@gmail.com>
2026-08-17 15:54:40 -0300

    ipfarre

--- views/banco_de_dados.py
+++ views/banco_de_dados.py
@@ -1,4 +1,11 @@
-from scripts.auxiliar import consultar_juiz
+from scripts.auxiliar import ler_google_sheets
 import streamlit as st
 
 st.write("pagina banco de dados")
+
+df = ler_google_sheets(
+    spreadsheet_id="1obdFvz7z50Ejzjv9NeYhZNV-OFbxLG_cHRRtJUIJn80",
+    nome_aba="base de personagens",
+)
+
+st.dataframe(df)
