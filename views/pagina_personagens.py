4791c3b3d714d886ffc7d4272ddcaed2ee6a3b8e
lucaswells007-dotcom <lucaswells007@gmail.com>
2026-08-24 16:02:46 -0300

    uofcare

--- views/pagina_personagens.py
+++ views/pagina_personagens.py
@@ -2,14 +2,18 @@ from scripts.auxiliar import ler_google_sheets
 import streamlit as st
 import pandas as pd
 
+# Initialization
+if 'df_personagem' not in st.session_state:
+    df_personagem = ler_google_sheets(
+                        spreadsheet_id="1obdFvz7z50Ejzjv9NeYhZNV-OFbxLG_cHRRtJUIJn80",
+                        nome_aba="base de personagens",
+                    )
+    st.session_state['df_personagem'] = df_personagem
+
+else:
+    df_personagem = st.session_state['df_personagem']
+
 st.title("conheça seu personagem")
-
-df_personagem = ler_google_sheets(
-    spreadsheet_id="1obdFvz7z50Ejzjv9NeYhZNV-OFbxLG_cHRRtJUIJn80",
-    nome_aba="base de personagens",
-)
-
-
 
 event = st.dataframe(
     df_personagem,
