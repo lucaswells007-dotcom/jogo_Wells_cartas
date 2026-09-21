a125a58e6902b74abf45b4dc4f2fe8841e9fe819
lucaswells007-dotcom <lucaswells007@gmail.com>
2026-09-21 15:26:05 -0300

    upfate

--- views/tela_principal.py
+++ views/tela_principal.py
@@ -1,11 +1,30 @@
 from scripts.auxiliar import ler_google_sheets
 import streamlit as st
 
-st.write("pagina banco de dados")
+if 'df_personagem' not in st.session_state:
+    df_personagem = ler_google_sheets(
+                        spreadsheet_id="1obdFvz7z50Ejzjv9NeYhZNV-OFbxLG_cHRRtJUIJn80",
+                        nome_aba="base de personagens",
+                    )
+    st.session_state['df_personagem'] = df_personagem
 
-df = ler_google_sheets(
-    spreadsheet_id="1obdFvz7z50Ejzjv9NeYhZNV-OFbxLG_cHRRtJUIJn80",
-    nome_aba="base de personagens",
-)
+else:
+    df_personagem = st.session_state['df_personagem']
 
-st.dataframe(df)
+if 'df_problemas' not in st.session_state:
+    df_problemas = ler_google_sheets(
+                        spreadsheet_id="1obdFvz7z50Ejzjv9NeYhZNV-OFbxLG_cHRRtJUIJn80",
+                        nome_aba="problemas",
+                    )
+    st.session_state['df_problemas'] = df_problemas
+
+else:
+    df_problemas = st.session_state['df_problemas']
+
+st.title('meu jogo')
+
+botao_problema = st.button('escolher problema')
+
+if botao_problema:
+    problema = df_problemas['problema'].sample(1).values[0]
+    st.write(problema)
